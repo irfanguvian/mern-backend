6407c1ef2a8ff9c2deb7a102a6843b04a3069cd0
irfanguvian <guvian14@gmail.com>
2021-09-22 22:36:15 +0700

    finished work

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 // import moogoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
@@ -39,6 +40,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/sb-admin-2",
