aa7362787a7008a3bcc0ec66dd8e3b54bb7319ce
irfanguvian <guvian14@gmail.com>
2022-07-02 20:45:43 +0700

    fix

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const flash = require("connect-flash");
 const cors = require("cors");
 // import moogoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+	"mongodb+srv://admin:admin123@cluster0.5ij1i.mongodb.net/db-staycation?retryWrites=true&w=majority",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	},
+);
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -28,12 +31,12 @@ app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-  })
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 60000 },
+	}),
 );
 app.use(flash());
 app.use(logger("dev"));
@@ -43,8 +46,10 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  "/sb-admin-2",
-  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+	"/sb-admin-2",
+	express.static(
+		path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"),
+	),
 );
 
 app.use("/", indexRouter);
@@ -53,18 +58,18 @@ app.use("/admin", adminRouter);
 app.use("/api/v1", apiRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
